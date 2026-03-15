0a9dee8aa491967d01dd5113a272cce3efd3f533
Eyüp <epsilonelittr@gmail.com>
2026-03-16 01:12:48 +0300

    feat(nx-react-code-viewer): add Storybook manager theme config
    Add manager.ts to configure dark theme with custom brand title for
    React code viewer Storybook instance.

---
+++ packages/client/ui/nx-react-code-viewer/.storybook/manager.ts
@@ -0,0 +1,11 @@
+import { addons } from 'storybook/manager-api';
+import { create } from 'storybook/theming';
+
+const customTheme = create({
+  base: 'dark',
+  brandTitle: 'NX React Code Viewer',
+});
+
+addons.setConfig({
+  theme: customTheme,
+});
